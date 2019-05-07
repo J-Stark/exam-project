aa3ffbfd7c1d04068b1eadf3b32d2bf1f3cbe071
J-Stark <jesper.stark@gmail.com>
2019-05-07 08:36:27 +0200

    Add files via upload

---
+++ Flask-SQL/db.py
@@ -0,0 +1,14 @@
+def user():
+    return "postgres"
+
+def password():
+    return "password"
+
+def host():
+    return "localhost"
+
+def port():
+    return "5432"
+
+def dbName():
+    return "baj"
